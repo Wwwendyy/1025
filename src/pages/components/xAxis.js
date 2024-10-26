c3ef052038d0d04ef29051cbf0d12fdbc7acd7c8
wwendy W <wwendyw@wwendydeMacBook-Air.local>
2024-10-26 14:21:44 +0800

    Please run

--- src/pages/components/xAxis.js
+++ src/pages/components/xAxis.js
@@ -43,7 +43,7 @@ function XAxis(props) {
             <g ref={axisRef}></g>
                 <text
                     style={{ textAnchor: 'end', fontSize: '18px' }}
-                    transform={`translate(${width}/2, -10)`}
+                    transform={`translate(${width}, -10)`}
                 >
                     {axisLabel}
                 </text>
