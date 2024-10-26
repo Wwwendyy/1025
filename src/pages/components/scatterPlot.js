75d2e3e4b4a8e670987a803f5f585a04d2084aab
wwendy W <wwendyw@wwendydeMacBook-Air.local>
2024-10-26 15:28:43 +0800

    Please run

--- src/pages/components/scatterPlot.js
+++ src/pages/components/scatterPlot.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import Points from './points';
-import YAxis from './yAxis';
 import XAxis from './xAxis';
+import YAxis from './yAxis';
 
 function ScatterPlot(props) {
-    const { offsetX, offsetY, data, xScale, yScale,
-            height, width, infoStation, onMouseEnter, onMouseOut,
-            setTooltipData, setTooltipPos } = props;
-    return (
-        <g transform={'translate(${offsetX}, ${offsetY})'}>
-            <Points
-                data={data}
-                xScale={xScale}
-                yScale={yScale}
-                height={height}
-                width={width}
-                infoStation={infoStation}
-                onMouseEnter={onMouseEnter}
-                onMouseOut={() => setTooltipData(null)}
-                setTooltipData={setTooltipData}
-                setTooltipPos={setTooltipPos}
-            />
-            <YAxis
-                yScale={yScale}
-                height={height}
-                axisLabel={"Trip duration end in"}
-            />
-            <XAxis
-                xScale={xScale}
-                height={height}
-                width = {width}
-                axisLabel={"Trip duration start from"}
-            />
-        </g>
-    );
+  const {
+    offsetX,
+    offsetY,
+    data,
+    xScale,
+    yScale,
+    height,
+    width,
+    infoStation,
+    onMouseEnter,
+    onMouseOut,
+    setTooltipData,
+    setTooltipPos,
+  } = props;
+
+  return (
+    <g transform={`translate(${offsetX}, ${offsetY})`}>
+      <Points
+        data={data}
+        xScale={xScale}
+        yScale={yScale}
+        height={height}
+        width={width}
+        infoStation={infoStation}
+        onMouseEnter={onMouseEnter}
+        onMouseOut={onMouseOut}
+        setTooltipData={setTooltipData}
+        setTooltipPos={setTooltipPos}
+      />
+      <YAxis yScale={yScale} height={height} axisLabel="Trip duration end in" />
+      <XAxis
+        xScale={xScale}
+        height={height}
+        width={width}
+        axisLabel="Trip duration start from"
+      />
+    </g>
+  );
 }
-export default ScatterPlot
+
+export default ScatterPlot;
