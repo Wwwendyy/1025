a2ff23afc1ec243eb1d52f9ccdb53a83e9051217
wwendy W <wwendyw@wwendydeMacBook-Air.local>
2024-10-26 14:25:25 +0800

    Please run

--- src/pages/components/yAxis.js
+++ src/pages/components/yAxis.js
@@ -1,14 +1,14 @@
 import React, { useRef, useEffect } from 'react';
-import { axisLeft } from 'd3-axis'; // Import axisLeft from d3-axis
-import { select } from 'd3-selection';
+import {select, axisLeft} from 'd3'; // Import axisLeft from d3-axis
+
 function YAxis(props) {
     const { yScale, height, axisLabel } = props; // Correct axisLabel spelling
     const axisRef = useRef(null); // Use useRef here
 
     useEffect(() => {
         if (yScale) {
-            const isLinear = typeof yScale.domain()[0] === 'number';
             const axis = axisLeft(yScale);
+            //const isLinear = typeof yScale.domain()[0] === 'number';
             select(axisRef.current).call(axis);
         }
     }, [yScale]); // Only depend on yScale
