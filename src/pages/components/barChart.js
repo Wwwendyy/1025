4dfbae84df7c86938d406c320ec9f0c2205732fc
wwendy W <wwendyw@wwendydeMacBook-Air.local>
2024-10-26 11:28:24 +0800

    Please run

--- src/pages/components/barChart.js
+++ src/pages/components/barChart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import XAxis from './xAxis';
 import YAxis from './yAxis';
 import Bars from './bars';
-import { TurborepoAccessTraceResult } from 'next/dist/build/turborepo-access-trace';
+
 function BarChart(props){
     const {offsetX, offsetY, data, xScale, yScale, height, width} = props;
     //task1: transform the <g> with the offsets so that the barchart can show properly 
